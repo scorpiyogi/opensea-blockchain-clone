d5b15823452ff32804377c0ec48e1ef450fbef59
scorpiyogi <sohani.codes@gmail.com>
2022-05-11 18:54:39 +0530

    Adding entire app

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 17,
+    from: '0xScorpi',
+    to: '0xAnni',
+    date: '4 months ago',
+  },
+  {
+    price: 17,
+    from: '0xScorpi',
+    to: '0xAnni',
+    date: '4 months ago',
+  },
+  {
+    price: 17,
+    from: '0xScorpi',
+    to: '0xAnni',
+    date: '4 months ago',
+  },
+  {
+    price: 17,
+    from: '0xScorpi',
+    to: '0xAnni',
+    date: '4 months ago',
+  },
+]
